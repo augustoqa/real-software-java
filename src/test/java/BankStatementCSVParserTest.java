d8133f35bb33d229a408f94c0ef2cc1990c9d581
Augusto Acual <augustoacual@gmail.com>
2023-06-20 11:41:14 -0600

    Test a line is correctly parse

--- src/test/java/BankStatementCSVParserTest.java
+++ src/test/java/BankStatementCSVParserTest.java
@@ -1,7 +1,11 @@
 import com.chapter_02.BankStatementCSVParser;
 import com.chapter_02.BankStatementParser;
+import com.chapter_02.BankTransaction;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 public class BankStatementCSVParserTest {
 
@@ -9,6 +13,16 @@ public class BankStatementCSVParserTest {
 
     @Test
     public void shouldParseOneCorrectLine() throws Exception {
-        Assert.fail("Not yet implemented");
+        final String line = "30-01-2017,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected =
+                new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
     }
 }
