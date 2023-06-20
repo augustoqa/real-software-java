0a0f1862ad06f4b1cdb8dd839326dbf276d7e8d8
Augusto Acual <augustoacual@gmail.com>
2023-06-20 11:27:30 -0600

    Configuration JUnit

---
+++ src/test/java/BankStatementCSVParserTest.java
@@ -0,0 +1,14 @@
+import com.chapter_02.BankStatementCSVParser;
+import com.chapter_02.BankStatementParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assert.fail("Not yet implemented");
+    }
+}
